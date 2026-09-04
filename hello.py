49cccbd159ca26a383e876deb5e3184d73cc0a24
Darryl Lim <2601735@sit.singaporetech.edu.sg>
2026-09-04 16:33:41 +0800

    Updated with username,bio, followers

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("=====================")
 print("Welcome here")
 print("My first post!")
 print("=====================")
+
+username = "yes"
+bio = "glhf"
+followers = "12"
+
+print(f'Username : {username}')
+print(f'Bio : {bio}')
+print(f'Followers : {followers}')
