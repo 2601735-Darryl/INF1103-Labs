cf70069b1647e7d2b8935aeff81a6935448b1eca
Darryl Lim <2601735@sit.singaporetech.edu.sg>
2026-09-11 17:00:55 +0800

    Updated auditor.py with reporting pressed units and rejected entries

--- auditor.py
+++ auditor.py
@@ -1,7 +1,9 @@
 inventory = 0
+failed_entries = 0
 while True:
     stock = input("Enter stock quantity (type 'q' to quit): ")
     if stock == 'q':
+        print(f"Total units processed: {inventory}\nNumber of Failed/Rejected entries: {failed_entries}")
         break
     elif inventory > 500:
         print("Inventory exceeding 500 units")
@@ -12,4 +14,5 @@ while True:
                 break
             print(inventory)
     else:
+        failed_entries += 1
         print("Error! Please enter a positive integer!")
