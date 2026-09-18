a2ef727d73c247014cb907379aa642fb50d4832c
Darryl Lim <2601735@sit.singaporetech.edu.sg>
2026-09-18 16:46:24 +0800

    Added main logic for modular_auditor.py

--- modular_auditor.py
+++ modular_auditor.py
@@ -18,3 +18,20 @@ def calculate_tax(amount):
 def generate_report(total_units, failed_attempts):
     print(f"Total units processed: {total_units}\nNumber of Failed/Rejected entries: {failed_attempts}")
     return
+
+inventory = 0
+failed_entries = 0
+delivery_fee = 0
+
+while True:
+    user_input = get_valid_input()
+    if user_input != None:
+        if user_input == 'q':
+            generate_report(inventory,failed_entries)
+            calculate_tax(inventory*5) # delivery for each unit is $5
+            break
+        elif user_input.isdigit():
+            inventory = process_delivery(inventory, int(user_input))
+            print(f'Current inventory: {inventory}')
+    else:
+        failed_entries += 1
